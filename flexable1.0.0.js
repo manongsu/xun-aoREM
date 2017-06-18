76925d4f0e3057edf74695e67c051d94ad9c2c34
manongsu <manongsu@163.com>
2017-06-18 17:18:04 +0800

    1.0.0

--- flexable1.0.0.js
+++ flexable1.0.0.js
@@ -2,8 +2,9 @@ function flex() {
     var width = $(window).width();
     if(width<=540){
         $("html").css("fontSize",width/10);
+    }else if(width>540){
+        $("html").css("fontSize",76.8);
     }
-
 }
 flex();
 $(window).resize(function () {
